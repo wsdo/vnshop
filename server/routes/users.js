209dd3506b74d6f0bd1971f15d1a99b53d12bcdc
stark <wsd312@163.com>
2017-09-29 10:05:51 +0800

    ll

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
+
+router.post('/login', function(req, res, next) {
+    let param = {
+            userName: req.body.userName,
+            userPwd: req.body.userPwd
+        }
+        // 把用户名和密码，去数据库查询，看看是否存在
+    User.findOne(param, function(err, doc) {
+        if (err) { res.json({ status: 1, msg: '用户名或密码错误' }) } else {
+            res.cookie('userId', doc.userId, {
+                path: '/',
+                maxAge: 1000 * 60 * 60
+            })
+
+            res.cookie('userName', doc.userName, {
+                path: '/',
+                maxAge: 1000 * 60 * 60
+            })
+
+
+            if (doc) {
+                res.json({
+                    status: 0,
+                    msg: '',
+                    result: {
+                        userName: doc.userName
+                    }
+                })
+            }
+        }
+    })
+})
 
 module.exports = router;
