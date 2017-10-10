aeb28f0cb7a2f250e0553022d87f8a3fcce888c5
stark <wsd312@163.com>
2017-10-10 15:04:13 +0800

    init

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,9 +3,9 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import GoodsList from '@/view/GoodsList'
 import User from '@/view/user/index'
-import Cart from '@/view/cart'
-import Address from '@/view/address'
-import OrderConfirm from '@/view/orderConfirm'
+import Cart from '@/view/Cart'
+import Address from '@/view/Address'
+import OrderConfirm from '@/view/OrderConfirm'
 import OrderSuccess from '@/view/OrderSuccess'
 
 Vue.use(Router)
