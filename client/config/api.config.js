0520137c5d7fd384ca71887842450abce5dd82ab
stark <wsd312@163.com>
2017-10-11 18:01:49 +0800

    init

--- client/config/api.config.js
+++ client/config/api.config.js
@@ -1,5 +1,5 @@
 const isProdMode = Object.is(process.env.NODE_ENV, 'production')
 
 module.exports = {
-    baseUrl: isProdMode ? 'http://misssu.cn/api/' : 'api/'
+    baseUrl: isProdMode ? 'http://vnshop.shudong.wang/api/' : 'api/'
 }
